b03a443bb40b9ecbdef40db2980c087b08a8ec9b
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2023-12-19 21:09:21 +0200

    Update add.js

--- controllers/hydrationEntries/add.js
+++ controllers/hydrationEntries/add.js
@@ -5,6 +5,7 @@ const add = async (req, res, next) => {
   const { _id: owner } = req.user;
 
   const result = await HydrationEntry.create({ ...req.body, owner });
+  result.owner = undefined;
 
   res.status(201).json(result);
 };
